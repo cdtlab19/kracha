0a384c636b30fc10f923371f1123c1eb4eb88e37
Cleanderson Lins <cleandersonlins@gmail.com>
2019-07-10 18:50:56 -0300

    Added a chaincode router

--- chaincode/cardholder/cardholder.go
+++ chaincode/cardholder/cardholder.go
@@ -1,13 +1,39 @@
 package chaincode
 
 import (
+	"github.com/cdtlab19/kracha/router"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/protos/peer"
 )
 
+//CardholderChaincode abstracts chaincode calls
 type CardholderChaincode struct {
+	logger *shim.ChaincodeLogger
+	router *router.Router
 }
 
-func (c *CardholderChaincode) Init(stub shim.ChaincodeStubInterface) peer.Response {
+//NewCardholderChaincode creates a new Chaincode with it's predefined routes
+func NewCardholderChaincode(logger *shim.ChaincodeLogger) *CardholderChaincode {
+	chaincode := &CardholderChaincode{
+		logger: logger,
+	}
+	chaincode.router.Handle("CreateUser", chaincode.CreateUser).
+		Handle("GetUset", chaincode.GetUser)
 
+	return chaincode
+}
+
+//Init implements CardholderChaincode basic setup
+func (c *CardholderChaincode) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	return shim.Success(nil)
+}
+
+//CreateUser initiates a basic user defined in model
+func CreateUser(stub shim.ChaincodeStubInterface) peer.Response {
+	//TODO
+}
+
+//GetUser returns a user based on it's CPF
+func GetUser(stub shim.ChaincodeStubInterface) peer.Response {
+	//TODO
 }
---
+++ router/handler.go
@@ -0,0 +1,33 @@
+package router
+
+import (
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
+)
+
+//Handler defines a Hyperledger Fabric basic type function
+type Handler func() peer.Response
+
+//Router handles all Chaincode methods via Invoke
+type Router struct {
+	routes map[string]Handler
+}
+
+//NewRouter creates a basic Chaincode routes
+func NewRouter() *Router {
+	return &Router{
+		routes: make(map[string]Handler),
+	}
+}
+
+//Handle adds a route to the Router.
+//Routes can be overwrited in case on name conflicting
+func (r *Router) Handle(method string, handler Handler) *Router {
+	r.routes[method] = handler
+	return r
+}
+
+//Invoke returns a Chaincode method based on it's initial name
+func (r *Router) Invoke(method string, stub shim.ChaincodeStubInterface) peer.Response {
+
+}
